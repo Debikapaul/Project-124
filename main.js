8fd42e68ba93abd5dfaa935e43ddb6928dbca949
Debikapaul <82105390+Debikapaul@users.noreply.github.com>
2021-11-18 22:10:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function setup(){
+    video=createCapture(VIDEO);
+    video.size(550,500);
+
+    canvas=createCanvas(450,400);
+    canvas.position(560,150);
+
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotposes);
+}
+
+function modelLoaded(){
+    console.log('PoseNet is Initialized!');
+}
+
+function draw(){
+    background(	'#0ce8f0');
+}
+
+function gotposes(results){
+    if(results.length > 0){
+
+        console.log(results);
+
+    }
+
+}
